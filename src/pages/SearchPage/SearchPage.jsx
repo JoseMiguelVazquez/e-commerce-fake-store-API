4d8e8cf23618fdc610e9b5e5c9973de6258aaf85
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-06-28 09:05:00 -0600

    loading to search page

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,4 +1,5 @@
-import { useEffect } from 'react'
+/* eslint-disable react/jsx-indent */
+import { useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { useSearchContext } from '@/context/SearchContext'
 import { getAllItems } from '@/services/itemServices'
@@ -9,6 +10,7 @@ const SearchPage = () => {
   const { searchItems, setSearchItems, setSearchTerm } = useSearchContext()
   const navigate = useNavigate()
   const { query } = useParams()
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     setSearchTerm(query)
@@ -24,6 +26,7 @@ const SearchPage = () => {
             }
             return false
           }))
+          setLoading(false)
         }
       } catch (error) {
         console.log('Ocurrió un error: ' + error.message)
@@ -34,25 +37,33 @@ const SearchPage = () => {
 
   return (
     <>
-      <div className='container-fluid pt-4'>
-        <div>
-          <button className='btn' onClick={() => navigate(-1)}> &lt; Back</button>
-        </div>
-        <div className='row d-flex justify-content-center'>
-          <h3>Results for: "{query}"</h3>
-          {searchItems.length === 0 &&
-            <p className='mt-3'>No Items Matching your search</p>}
-          {searchItems.map((item) => (
-            <ItemCard
-              key={item.id}
-              id={item.id}
-              imageUrl={item.image}
-              name={item.title}
-              price={item.price}
-            />
-          ))}
-        </div>
-      </div>
+      {
+      loading
+        ? <div className='search-loading-div d-flex align-items-center justify-content-center'>
+            <div className='spinner-border' role='status'>
+              <span className='visually-hidden'>Loading...</span>
+            </div>
+          </div>
+        : <div className='container-fluid pt-4'>
+            <div>
+              <button className='btn' onClick={() => navigate(-1)}> &lt; Back</button>
+            </div>
+            <div className='row d-flex justify-content-center'>
+              <h3>Results for: "{query}"</h3>
+              {searchItems.length === 0 &&
+                <p className='mt-3'>No Items Matching your search</p>}
+              {searchItems.map((item) => (
+                <ItemCard
+                  key={item.id}
+                  id={item.id}
+                  imageUrl={item.image}
+                  name={item.title}
+                  price={item.price}
+                />
+              ))}
+            </div>
+          </div>
+    }
     </>
   )
 }
